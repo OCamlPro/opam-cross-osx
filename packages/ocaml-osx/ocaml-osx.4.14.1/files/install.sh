8e47ccc7e74fff361fdb237f383ec4f4bd715051
Nicolas Berthier <nicolas.berthier@ocamlpro.com>
2024-07-24 17:15:23 +0200

    ocaml-osx: use opam way to set `sysroot` variable

--- packages/ocaml-osx/ocaml-osx.4.14.1/files/install.sh
+++ packages/ocaml-osx/ocaml-osx.4.14.1/files/install.sh
@@ -3,7 +3,11 @@
 OPAM_PREFIX="$1"
 HOST="$2"
 
-echo "sysroot: \"${OPAM_PREFIX}/osx-sysroot\"" > ${OPAM_PREFIX}/.opam-switch/config/ocaml-osx.config
+cat > "ocaml-osx.config" <<EOF
+variables {
+  sysroot: "${OPAM_PREFIX}/osx-sysroot"
+}
+EOF
 
 for bin in ocamlc ocamlopt ocamlmklib; do
   ln -s "${OPAM_PREFIX}/osx-sysroot/bin/${bin}" "${OPAM_PREFIX}/bin/${HOST}-${bin}"
