9675fb82549c2183c028e903f78828e847d9dd05
David Declerck <david.declerck@ocamlpro.com>
2025-06-10 16:12:43 +0200

    Minor adjustments

--- packages/ocaml-osx/ocaml-osx.4.14.1/files/install.sh
+++ packages/ocaml-osx/ocaml-osx.4.14.1/files/install.sh
@@ -3,13 +3,6 @@
 OPAM_PREFIX="$1"
 HOST="$2"
 
-cat > "ocaml-osx.config" <<EOF
-opam-version: "2.0"
-variables {
-  sysroot: "${OPAM_PREFIX}/osx-sysroot"
-}
-EOF
-
 for bin in ocamlc ocamlopt ocamlmklib; do
   ln -s "${OPAM_PREFIX}/osx-sysroot/bin/${bin}" "${OPAM_PREFIX}/bin/${HOST}-${bin}"
 done
